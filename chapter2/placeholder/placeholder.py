eaefc4a0367454238a7e35dd533297393937ac39
piyo56 <piyo56.net@gmail.com>
2016-09-14 12:01:30 +0900

    [Update] update chapter2 contents #1

--- chapter2/placeholder/placeholder.py
+++ chapter2/placeholder/placeholder.py
@@ -25,24 +25,46 @@ from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from django import froms
+from django import forms
+from django.core.cache import cache
+from io import BytesIO
+from PIL import Image, ImageDraw
 
 class ImageForm(forms.Form):
     """Form to validate requested placeholder image."""
     height = forms.IntegerField(min_value=1, max_value=2000)
     width  = forms.IntegerField(min_value=1, max_value=2000)
 
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes"""
+        height  = self.cleaned_data['height']
+        width   = self.cleaned_data['width']
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image   = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop  = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        cache.set(key, content, 60 * 60)
+        return content
+
 def placeholder(request, width, height):
     form = ImageForm({'height': height, 'width': width})
 
     if form.is_valid():
-        height = form.cleaned_data['height']
-        width  = form.cleaned_data['width']
-        # TODO: Generate image of requested size
-        return HttpResponse('Ok')
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
     else:
         return HttpResponseBadRequest('Invalid Image Request')
-...
+
 def index(request):
     return HttpResponse('Hello World')
 
